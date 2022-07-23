a397a399de189a92c67e4e2b9f6fce56ca669026
Marco <5788988+marc0janssen@users.noreply.github.com>
2022-07-23 13:54:48 +0200

    .

--- install.sh
+++ install.sh
@@ -6,22 +6,22 @@ chmod +x /usr/local/bin/restic_backup.sh
 cp ./restic_cleanup.sh /usr/local/bin/restic_cleanup.sh
 chmod +x /usr/local/bin/restic_cleanup.sh
 
-sudo cp ./com.cbergoon.restic_backup_local.plist /Library/LaunchDaemons/com.cbergoon.restic_backup_local.plist
-sudo cp ./com.cbergoon.restic_backup_remote.plist /Library/LaunchDaemons/com.cbergoon.restic_backup_remote.plist
+sudo cp ./nl.mjanssen.restic_backup_local.plist /Library/LaunchDaemons/nl.mjanssen.restic_backup_local.plist
+sudo cp ./nl.mjanssen.restic_backup_remote.plist /Library/LaunchDaemons/nl.mjanssen.restic_backup_remote.plist
 
-sudo cp ./com.cbergoon.restic_cleanup_local.plist /Library/LaunchDaemons/com.cbergoon.restic_cleanup_local.plist
-sudo cp ./com.cbergoon.restic_cleanup_remote.plist /Library/LaunchDaemons/com.cbergoon.restic_cleanup_remote.plist
+sudo cp ./nl.mjanssen.restic_cleanup_local.plist /Library/LaunchDaemons/nl.mjanssen.restic_cleanup_local.plist
+sudo cp ./nl.mjanssen.restic_cleanup_remote.plist /Library/LaunchDaemons/nl.mjanssen.restic_cleanup_remote.plist
 
-cp ./private.restic_backup.conf ~/.restic_backup.conf
+cp ./.restic_backup.conf ~/.restic_backup.conf
 
-launchctl unload /Library/LaunchDaemons/com.cbergoon.restic_backup_local.plist
-launchctl load /Library/LaunchDaemons/com.cbergoon.restic_backup_local.plist
+launchctl unload /Library/LaunchDaemons/nl.mjanssen.restic_backup_local.plist
+launchctl load /Library/LaunchDaemons/nl.mjanssen.restic_backup_local.plist
 
-launchctl unload /Library/LaunchDaemons/com.cbergoon.restic_backup_remote.plist
-launchctl load /Library/LaunchDaemons/com.cbergoon.restic_backup_remote.plist
+launchctl unload /Library/LaunchDaemons/nl.mjanssen.restic_backup_remote.plist
+launchctl load /Library/LaunchDaemons/nl.mjanssen.restic_backup_remote.plist
 
-launchctl unload /Library/LaunchDaemons/com.cbergoon.restic_cleanup_local.plist
-launchctl load /Library/LaunchDaemons/com.cbergoon.restic_cleanup_local.plist
+launchctl unload /Library/LaunchDaemons/nl.mjanssen.restic_cleanup_local.plist
+launchctl load /Library/LaunchDaemons/nl.mjanssen.restic_cleanup_local.plist
 
-launchctl unload /Library/LaunchDaemons/com.cbergoon.restic_cleanup_remote.plist
-launchctl load /Library/LaunchDaemons/com.cbergoon.restic_cleanup_remote.plist
+launchctl unload /Library/LaunchDaemons/nl.mjanssen.restic_cleanup_remote.plist
+launchctl load /Library/LaunchDaemons/nl.mjanssen.restic_cleanup_remote.plist
